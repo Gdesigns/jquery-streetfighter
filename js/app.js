5df9b7b8827cb81fd6cd274c19558391f918e0c4
Greg Hoffman <greg@4walls.net>
2015-05-27 13:26:56 -0400

    Final animation

--- js/app.js
+++ js/app.js
@@ -25,33 +25,27 @@ $(document).ready(function() {
 		$('.ryu-throwing').hide();
 		$('.ryu-ready').show();
 	});
-//	.keydown(function(event) {
-//		if (event.which == 88 ) {
-//			$('.ryu-still').hide();
-//			$('.ryu-ready').hide();
-//			$('.ryu-throwing').hide();
-//			$('.ryu-cool').show();
-//		}
-//	});
 });
 
 $(document).keydown(function(event) {
-	if (event.which == 88 ) {
+	if (event.which == 88) {
+		// console.log('X key pressed');
 		$('.ryu-still').hide();
 		$('.ryu-ready').hide();
-		$('.ryu-throwing').hide();
 		$('.ryu-cool').show();
+		}
+	})
+	
+	.keyup(function(event) {
+	if (event.which == 88) {
+		// console.log('X key released');
+		$('.ryu-cool').hide();
+		$('.ryu-still').show();
 	}
 });
-$(document).keyup(function(event) {
-	if (event.which == 88) {
-		$('ryu-cool').hide();
-		$('ryu-still').show();
-	}
-})
 
 function playHadouken () {
-	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].volume = 0.2;
 	$('#hadouken-sound')[0].load();
 	$('#hadouken-sound')[0].play();
 }
